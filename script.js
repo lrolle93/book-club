8696a43c44289aa535c0f0365991f977623fa6f1
lrolle93 <71038516+lrolle93@users.noreply.github.com>
2020-11-11 15:13:52 -0500

    Popup & Javascript
    Edited HTML, CSS, created JS for popup

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('hey there')
+
+function showpopup(){
+	var popup = document.getElementById('popup')
+	popup.classList.add('show')
+}
+
+function hidepopup(){
+	var popup = document.getElementById('popup')
+	popup.classList.remove('show')
+}
+
